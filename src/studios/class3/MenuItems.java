66d96759ff31791e8eed6159b6f101e58e4855e3
Sean <Seanpf12@hotmail.com>
2023-04-06 20:03:05 -0500

    Finished Class 3 studio

--- src/studios/class3/MenuItems.java
+++ src/studios/class3/MenuItems.java
@@ -1,5 +1,6 @@
 package studios.class3;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -9,9 +10,72 @@ public class MenuItems {
     private String category;
     private Boolean isNew;
     private double price;
-    private boolean isVegan;
-    private boolean isAvailable;
-    private Date dateAdded;
-    private ArrayList<String> allergens;
+    private LocalDate dateAdded;
     private int spiceLevel;
+
+    public MenuItems(String description, String name, String category,
+                     double price, LocalDate dateAdded, int spiceLevel) {
+        this.description = description;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.dateAdded = dateAdded;
+        this.spiceLevel = spiceLevel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public int getSpiceLevel() {
+        return spiceLevel;
+    }
+
+    public void setSpiceLevel(int spiceLevel) {
+        this.spiceLevel = spiceLevel;
+    }
 }
